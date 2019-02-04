d77421c5fb8e346c29f208b0c44e143632e056bf
Marto_0 <martvanenck1@gmail.com>
2019-02-04 20:51:09 +0100

    fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/update', (req, res, next) => {
 
 
 app.listen(process.env.PORT || port, () => {
-  console.log('Listening at: ' + this.address())
+  console.log('Server started')
 })
 
 // connect database
